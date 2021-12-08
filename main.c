a22c2ecf39a68fad82d9f06d3bf26be3ddc89028
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-12-08 20:07:26 +0200

    Lesson 2 - User Input

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+void myMethod();
+
+int sum(int x, int y);
+
 int main() {
-    printf("Hello, World!\n");
+    int userInput1, userInput2,sumOfInput;
+
+    printf("Give your first number: ");
+    scanf("%d", &userInput1);
+
+    printf("Give your second number: ");
+    scanf("%d", &userInput2);
+
+    sumOfInput = sum(userInput1,userInput2);
+    printf("The sum of %d and %d is: %d",userInput1,userInput2,sumOfInput);
+
     return 0;
+}
+
+void myMethod() {
+    printf("Hello\n");
+}
+
+int sum(int x, int y) {
+    return x + y;
 }
