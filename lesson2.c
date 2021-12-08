e1138c58d7a365241284c62a6a53b89987a4d1f8
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-12-08 22:03:14 +0200

    Lesson 2 - End

--- lesson2.c
+++ lesson2.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 /*
- *
+ * This was a C lesson 2 by Alepis Efthymios on 8/12.
+ * Topics Discussed: User Input |  User Output | If-else if - else | For, while, do-while loops
+ */
+
+/*
+ * Methods should be declared at the top. If you want to use them further in the code, then declare them and
+ * write your logic later on.
  */
 
 void myMethod();
@@ -17,18 +23,35 @@ void performWhileLoop(int userChoice);
 void performDoWhileLoop(int userChoice);
 
 int main() {
+//    int userInput1, userInput2,sumOfInput;
+//
+//    printf("Give your first number: ");
+//    scanf("%d", &userInput1);
+//
+//    printf("Give your second number: ");
+//    scanf("%d", &userInput2);
+//
+//    sumOfInput = sum(userInput1,userInput2);
+//    evaluateExpression(sumOfInput);
+//
+//    printf("The sum of %d and %d is: %d\n ",userInput1,userInput2,sumOfInput);
+
     int userChoice;
     printf("Give a number");
     scanf("%d", &userChoice);
 
     performWhileLoop(userChoice);
 
-    //performForLoop();
+    //performForLoop(userChoice);
 
-    //performDoWhileLoop();
+    //performDoWhileLoop(userChoice);
 
     return 0;
 }
+
+/*
+ * Simple method printing a hello output to console & sum 2 integers
+ */
 
 void myMethod() {
     printf("Hello\n");
@@ -38,10 +61,14 @@ int sum(int x, int y) {
     return x + y;
 }
 
+/*
+ * If - else if - else statement in C language
+ */
+
 void evaluateExpression(int sum) {
-    if (sum > 10) {
+    if(sum > 10){
         printf("The sum is greater than 10");
-    } else if (sum < 10) {
+    }else if(sum < 10){
         printf("The sum is less than 10");
     } else {
         printf("The sum is equal to 10");
